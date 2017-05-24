9316895cccbe2ebe048f6242f9a7e087c79b7bd1
kmliuhoujun@outlook.com <kmliuhoujun@outlook.com>
2017-05-23 17:10:25 -0700

    Shopping Mode Goodness Made a working Shopping Mode Interface. Yet to add more database work, NOT WORKING FULLY YET.

--- ShoppingMode.cs
+++ ShoppingMode.cs
@@ -2,6 +2,7 @@ using Foundation;
 using System;
 using UIKit;
 using System.Collections.Generic;
+using System.Linq;
 namespace SCCiPhone
 {
    
@@ -15,6 +16,7 @@ namespace SCCiPhone
         }
         void Refresh()
         {
+            
 			string[] data = new string[ID + 1];
             string[] amount = new string[ID + 1];
 			for (int i = ID - 1; i > -1; i--)
@@ -27,22 +29,49 @@ namespace SCCiPhone
 			}
 
 
-            Recipt.Source = new ShoppingTavl(data, amount, Recipt);
+            ShoppingTavl source = new ShoppingTavl(data, amount, Recipt, amounts, names, ID);
+            Recipt.Source = source;
+            source.DeletedTrans += (object sender, EventArgs e) => removeAtRow(source.Index);
+            Recipt.ReloadData();
+        }
+        void removeAtRow(int row)
+        {
+			var temp1 = amounts.ToDictionary(entry => entry.Key,
+											   entry => entry.Value);
+			var temp2 = names.ToDictionary(entry => entry.Key,
+											   entry => entry.Value);
+            for (int i = row+1; i <= temp1.Keys.Max(); i++)
+            {
+                
+                    amounts.Remove(i-1);
+                    amounts.Add(i - 1, temp1[i]);
+                
+            }
+			for (int i = row + 1; i <= temp2.Keys.Max(); i++)
+			{
+                
+				names.Remove(i-1);
+				names.Add(i - 1, temp2[i]);
+				
+			}
+			
+			Recipt.ReloadData();
+		
         }
         partial void UIButton6081_TouchUpInside(UIButton sender)
         {
-            
-                float amountL = float.Parse(amount.Text);
-                string itemL = item.Text;
-                float quantity = float.Parse(quant.Text);
-                float discountL = float.Parse(discont.Text);
-                names.Add(ID, quantity.ToString() + " of " + item);
-                if (discountL.ToString() != "" & discountL.ToString() != "0")
-                {
-                    discountL = 0;
-                }
-                amounts.Add(ID, (amountL * quantity) - discountL * amountL);
-                Refresh();
+            float amountL = float.Parse(amount.Text);
+            string itemL = item.Text;
+            float quantity = float.Parse(quant.Text);
+            float discountL = 0;
+            names.Add(ID, quantity.ToString() + " of " + item.Text);
+            if (discont.Text != "" & discont.Text != "0")
+            {
+                discountL = float.Parse(discont.Text);
+            }
+            amounts.Add(ID, (amountL * quantity) - (discountL/100) * amountL);
+            ID += 1;
+            Refresh();
             
         }
     }
--- ShoppingTavl.cs
+++ ShoppingTavl.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿﻿using System;
 using UIKit;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 namespace SCCiPhone
 {
 	public class ShoppingTavl : UITableViewSource
@@ -8,15 +11,44 @@ namespace SCCiPhone
 		protected string cellId = "TableCell";
 		protected string[] TableSubtitles;
 		protected UITableView tbv;
-		public ShoppingTavl(string[] items, string[] subtitles, UITableView maintbl)
+		Dictionary<int, float> amounts = new Dictionary<int, float>();
+		Dictionary<int, string> names = new Dictionary<int, string>();
+        public int Index;
+        int length;
+        public event EventHandler DeletedTrans;
+        int ID;
+		public ShoppingTavl(string[] items, string[] subtitles, UITableView maintbl, Dictionary<int, float> _amounts, Dictionary<int, string> _names, int _ID)
 		{
 			tbv = maintbl;
 			TableItems = items;
 			TableSubtitles = subtitles;
+            amounts = _amounts;
+            names = _names;
+            ID = _ID;
+            length = _amounts.Keys.Max();
+		}
+		void Refresh()
+		{
+            tbv.ReloadData();
+			string[] data = new string[ID + 1];
+			string[] amount = new string[ID + 1];
+			for (int i = ID - 1; i > -1; i--)
+			{
+
+				string amounted = amounts[i].ToString();
+				string thing = names[i];
+				data[i] = thing;
+				amount[i] = "$" + amounted;
+			}
+
+
+			tbv.Source = new ShoppingTavl(data, amount, tbv, amounts, names, ID);
+
+			tbv.ReloadData();
 		}
 		public override nint RowsInSection(UITableView tableview, nint section)
 		{
-			return TableItems.Length;
+            return length;
 		}
 		public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
 		{
@@ -31,6 +63,24 @@ namespace SCCiPhone
 			tableView.DeselectRow(indexPath, true);
 
 		}
+        public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
+        { 
+            switch (editingStyle)
+            {
+                case UITableViewCellEditingStyle.Delete:
+                    Refresh();
+					DeletedTrans(this, EventArgs.Empty);
+					Index = indexPath.Row;
+                    tableView.DequeueReusableCell("TableCell");
+                    length -= 1;
+                    tableView.DeleteRows(new Foundation.NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+                    Refresh();
+                    break;
+				case UITableViewCellEditingStyle.None:
+					Console.WriteLine("SCCSTATUS: CommitEditingStyle: None called");
+					break;
+            }
+        }
 
 	}
 }
